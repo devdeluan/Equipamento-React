23c775873e37ff338d2955faa77caeac08da609c
Denis luan Sales de Santana <394.299.878-52@info.local>
2023-09-05 21:48:12 -0300

    05-09-SENAI!

--- src/pages/Equipamento/index.tsx
+++ src/pages/Equipamento/index.tsx
@@ -3,27 +3,31 @@ import api from "../../utils/api";
 import { useEffect, useState } from "react"
 import './style.css'
 
+
+export default Equipamento;    
+
 function Equipamento() {
 
   const [equip, setEquip] = useState<any[]>([
 
   ])
 
-  //state equips com os filtros definidos
-  const [equips, setequips] = useState<string[]>(
+  //state filtros com os filtros definidos
+  const [filtros, setFiltros] = useState<string[]>(
     [
       "ID",
       "MODELO",
       "FABRICANTE",
       'DATA',
       "CONSUMO",
-      'VALOR',
+      'VALOR'
     ]);
+    
   const [select, setSelect] = useState<string>(""); // state que contém a opção de filtro selecionado pelo usuário
 
-  const [Idgititado, setIdgititado] = useState<string>("");
+  const [filtroDigitado, setfiltroDigitado] = useState<string>("");
 
-  const [listaIdFiltrados, setlistaIdFiltrados] = useState<any[]>(equip);
+  const [listaBuscaFiltrado, setlistaBuscaFiltrado] = useState<any[]>(equip);
 
   useEffect(() => {
     document.title = "Lista de Equipamentos"
@@ -32,23 +36,41 @@ function Equipamento() {
 
   }, [])
 
-  function buscarPorId(event: any) {
+  function buscarPor(event: any) {
     event.preventDefault();
 
-    const idsFiltrados = equip.filter((eq: any) => eq.id.includes(Idgititado.toLocaleUpperCase()))
-    if (idsFiltrados.length === 0) {
+    const equipFiltrado = equip.filter((eq: any) => eq.id.includes(filtroDigitado.toLocaleUpperCase()))
+    if (equipFiltrado.length === 0) {
       alert("Nenhum Id encontrado!!")
     }
+    else if (equipFiltrado == equip.filter((eq: any) => eq.id.includes(filtroDigitado.toLocaleUpperCase()))) {
+      setlistaBuscaFiltrado(equipFiltrado)
+    }
+    else if (equipFiltrado == equip.filter((eq: any) => eq.modelo.includes(filtroDigitado.toLocaleUpperCase()))) {
+      setlistaBuscaFiltrado(equipFiltrado)
+    }
+    else if (equipFiltrado == equip.filter((eq: any) => eq.fabricante.includes(filtroDigitado.toLocaleUpperCase()))) {
+      setlistaBuscaFiltrado(equipFiltrado)
+    }
+    else if (equipFiltrado == equip.filter((eq: any) => eq.data.includes(filtroDigitado.toLocaleUpperCase()))) {
+      setlistaBuscaFiltrado(equipFiltrado)
+    }
+    else if (equipFiltrado == equip.filter((eq: any) => eq.consumo.includes(filtroDigitado.toLocaleUpperCase()))) {
+      setlistaBuscaFiltrado(equipFiltrado)
+    }
+    else if (equipFiltrado == equip.filter((eq: any) => eq.valor.includes(filtroDigitado.toLocaleUpperCase()))) {
+      setlistaBuscaFiltrado(equipFiltrado)
+    }
     else {
-      setlistaIdFiltrados(idsFiltrados)
+      setlistaBuscaFiltrado(equipFiltrado)
     }
   }
 
   function retornoEquipGeral(event: any) {
     if (event.target.value === "") {
-      setlistaIdFiltrados(equip)
+      setlistaBuscaFiltrado(equip)
     }
-    setIdgititado(event.target.value)
+    setfiltroDigitado(event.target.value)
   }
 
   function listarEquipamentos() {
@@ -63,21 +85,30 @@ function Equipamento() {
   return (
     <>
       <main>
+        
         <section className="equipamentos">
+          
           <div className="painelEqpm">
+
+            <form method="post" onSubmit={buscarPor}>
+
             <select defaultValue={"DEFAULT"} name="" id="cad_select_skill" onChange={(e) => setSelect(e.target.value)}>
               <option selected value="DEFAULT" disabled>Selecione</option>
               {
-                equips.map((equip: any, index: number) => {
+                filtros.map((equip: any, index: number) => {
                   return <option key={index} value={equip}>{equip}</option>
                 })
               }
             </select>
-            
+
             <input className="buscarInput"
              type="search" 
-             placeholder="Buscar ..." 
+             placeholder="Buscar por filtros ..." 
+             onChange={retornoEquipGeral}
              />
+            <button type="submit">Buscar</button>
+
+             </form>
 
             <div className="menu2Bbtn">
               <a
@@ -107,7 +138,7 @@ function Equipamento() {
               <tbody>
                 <tr className= 'tabelaEquip'>
                   {
-                    listaIdFiltrados.length === 0 ?
+                    listaBuscaFiltrado.length === 0 ?
                       equip.map((equip: any, index: number) => {
                         return <td  key={index}>
 
@@ -122,7 +153,7 @@ function Equipamento() {
 
                             />
                           </td>
-            }) : listaIdFiltrados.map((equip: any, index: number) => {
+            }) : listaBuscaFiltrado.map((equip: any, index: number) => {
                             return <td  key={index}>
 
                               <CardEquipamento
@@ -149,4 +180,3 @@ function Equipamento() {
   )
 }
 
-export default Equipamento;    
