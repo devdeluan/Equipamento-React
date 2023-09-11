c68139bb07da224c815cdf5b79346784f1c3c5be
Deluan26 <deluan19951@gmail.com>
2023-09-11 18:14:24 -0300

    ok22

--- src/components/CardEquip/index.tsx
+++ src/components/CardEquip/index.tsx
@@ -15,18 +15,16 @@ export default function CardEquipamento(props:any){
     return(
         <>
                     <tbody>
+                    <input type="checkbox"/>
                         <tr className="linhaTabela">
+                            {/* <td><input type="checkbox"/></td> */}
                             <td className='id'>{props.id}</td>
                             <td className='modelo'>{props.modelo}</td>
                             <td className='fabricante'>{props.fabricante}</td>
                             <td className='data'>{props.data}</td>
                             <td className='consumo'>{props.consumo}(kWh)</td>
                             <td className='valor'>R$ {props.valor}</td>
-                            {/* <input
-                                type="checkbox"
-                                // checked={selectedRows.includes(equip.id)}
-                                // onChange={() => toggleRow   Selection(equip.id)}   
-                                                /> */}
+
                         </tr>
                     </tbody>
         </>
